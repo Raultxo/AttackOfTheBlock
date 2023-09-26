61e0da4328fc88d09e684ade2bd5b5ea8558af82
Raúl Estrada <raul6114@gmail.com>
2023-09-26 11:36:31 +0200

    Terminar juego

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,29 +7,23 @@ using Random = UnityEngine.Random;
 
 public class EnemyScript : MonoBehaviour
 {
-    private float speed = 3f;
     private int seed;
     private Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-        seed = Random.Range(1, 4);
+        // Un random que invierte la gravedad a aprox la mitad de los enemigos
+        seed = Random.Range(0, 2);
         rigidbody2d = GetComponent<Rigidbody2D>();
         switch(seed)
         {
+            case 0:
+                rigidbody2d.gravityScale = -1;
+                break;
             case 1:
-                transform.position = new Vector2(transform.position.x + speed * -1, transform.position.y);
+                rigidbody2d.gravityScale = 1;
                 break;
-            case 2:
-                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
-                break;
-            case 3:
-                transform.position = new Vector2(transform.position.x, transform.position.y + speed);
-                break;
-            case 4:
-                transform.position = new Vector2(transform.position.x, transform.position.y + speed * -1);
-                break;
-        }
+        } 
     }
 
     // Update is called once per frame
@@ -41,26 +35,7 @@ public class EnemyScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        switch (seed)
-        {
-            case 1:
-                speed++;
-                rigidbody2d.AddForce(transform.up * 1f,ForceMode2D.Force);
-                break;
-            case 2:
-                speed++;
-                speed++;
-                rigidbody2d.AddForce(transform.up * 2f,ForceMode2D.Force);
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
-        if (seed == 1)
-        {
-            
-        }
+        
         
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,11 +12,14 @@ using UnityEngine;
      // Use this for initialization
      void Start ()
      {
+         // Desaparecer cursor y hacer que no pueda salir de la ventana
          Cursor.visible = false;
+         Cursor.lockState = CursorLockMode.Confined;
      }
     
      // Update is called once per frame
      void Update () {
+         // El sprite sigue en todo momento el ratón, con un offset de 0.075f
          mousePosition = Input.mousePosition;
          mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
          transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
@@ -24,7 +27,11 @@ using UnityEngine;
 
      private void OnCollisionEnter2D(Collision2D other)
      {
-         Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+         // Si se choca con un enemigo, se acaba el juego
+         if (other.gameObject.tag == "Enemy")
+         {
+             Application.Quit();
+         }
          
          
      }
