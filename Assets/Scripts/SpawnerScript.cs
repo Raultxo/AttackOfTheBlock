272bd644717eba31da153111d174c39f6900a53f
Raul Estrada <raul6114@gmail.com>
2023-09-26 19:06:49 +0200

    Configurado que las bolas no puedan spawnear en el jugador

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -1,26 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnerScript : MonoBehaviour
 {
     [SerializeField] private GameObject bola;
+    [SerializeField] private GameObject jugador;
 
-    [SerializeField] private float tiempo;
+    private float tiempo = 7f;
     // Start is called before the first frame update
     void Start()
     {
+        // Llamar al metodo "Spawn" cada 5 segundos
         InvokeRepeating("Spawn", tiempo, tiempo);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void Spawn()
     {
-        GameObject.Instantiate(bola);
+        // Spawnear la bola de manera aleatoria controlando que no spawnee en el jugador
+        float posX = Random.Range(-9f, 9f);
+        float posY = Random.Range(-5f, 5f);
+
+        while (Vector2.Distance(jugador.transform.position, new Vector2(posX,posY)) < 3f)
+        {
+            posX = Random.Range(-9f, 9f);
+            posY = Random.Range(-5f, 5f);
+        }
+        GameObject.Instantiate(bola, new Vector2(posX, posY), Quaternion.identity);
     }
 }
